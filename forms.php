40e506fd6a5c89524c4e99258d6fa5ffb9fa8782
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-07-17 07:35:02 +0000

    enter name

--- forms.php
+++ forms.php
@@ -4,8 +4,8 @@
 </head>
 <body>
 
-<form>
-    <input type = "text">
+<form action = "process.php" method = "post"> <!-- action goes to the file -->
+    Enter your name:<input name = "name" type = "text"> <!-- In order to access this, you need name! -->
     <input type = "submit">
 </form>
 </body>
