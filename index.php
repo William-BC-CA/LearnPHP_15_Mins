b968935f60b8f6e7bf958dd84ddb70d900707180
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-07-17 07:27:53 +0000

    real concat

--- index.php
+++ index.php
@@ -8,15 +8,17 @@ Hello, World!
 
 <?php
 
-    $myvar = "This is my variable";
-    $number = 5;
-    $number2 = 3;
+    // $myvar = "This is my variable";
+    // $number = 5;
+    // $number2 = 3;
     // $sum = $number + $number2;
-    $sum = $myvar + $number2;
+    // $sum = $myvar + $number2; // DOESN't WORK! NOT CONCATENATION!
+    $name = "William";
 
     // echo "Hello World!";
     // echo $myvar;
-    echo $sum;
+    // echo $sum;
+    echo "Hello, " . $name;
 ?>
 </body>
 </html>
