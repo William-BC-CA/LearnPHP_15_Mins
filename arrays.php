04cdca965a06deb5e1dfa6489e4576d869dae141
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-07-17 07:39:59 +0000

    arrays!

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        
+        <?php
+
+            // $person1 = "Alice";
+            // $person2 = "Bob";
+
+            $people = array("Alice", "Bob", "Catherine");
+
+            print_r($people); // To Print Array
+
+        ?>
+
+    </body>
+</html>
