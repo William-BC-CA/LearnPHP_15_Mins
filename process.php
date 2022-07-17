88609533f861001619de4c0bd1fef976eccfc513
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-07-17 07:37:16 +0000

    process info!

---
+++ process.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        
+
+    <?php
+
+        $name = $_POST["name"]; // $_POST is an array!
+        echo "Hello, " . $name;
+        
+    ?>
+    </body>
+</html>
